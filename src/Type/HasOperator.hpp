01eaff429979357f63505ba3c9c020c34d8d8718
Baduit <simon.bertho@epitech.eu>
2019-09-29 17:55:40 +0200

    Base for the rest of operators

--- src/Type/HasOperator.hpp
+++ src/Type/HasOperator.hpp
@@ -1,3 +1,8 @@
 #pragma once
 
 #include "Operators/Arithmetic.hpp"
+#include "Operators/Bitwise.hpp"
+#include "Operators/Comparaison.hpp"
+#include "Operators/CompoundAssigment.hpp"
+#include "Operators/Logical.hpp"
+#include "Operators/Other.hpp"
