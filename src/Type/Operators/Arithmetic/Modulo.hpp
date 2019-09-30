8515cd5ec29c185d541fc9e22133444163374837
Baduit <simon.bertho@epitech.eu>
2019-09-30 20:48:59 +0200

    Arithmetic operators

--- src/Type/Operators/Arithmetic/Modulo.hpp
+++ src/Type/Operators/Arithmetic/Modulo.hpp
@@ -7,4 +7,35 @@
 namespace Thuto
 {
 
+template<typename Pair, typename _ = void>
+struct SfinaeHasOperator_modulo_with : std::false_type {};
+
+template<typename Pair>
+struct SfinaeHasOperator_modulo_with
+<
+	Pair,
+	std::conditional_t
+	<
+		false,
+		Details::SfinaeHelper
+		<
+            decltype(std::declval<Details::TypeIn<Pair, 0>>() % std::declval<Details::TypeIn<Pair, 1>>())
+		>,
+		void
+	>
+> : public std::true_type
+{
+};
+
+
+template <typename T, typename With>
+inline constexpr bool has_operator_modulo_with()
+{
+    if constexpr (std::is_base_of_v<std::true_type, SfinaeHasOperator_modulo_with<std::pair<T, With>>>)
+        return true;
+    else
+        return false;
+}
+
+
 }
