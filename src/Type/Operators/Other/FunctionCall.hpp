e343164f5d0a8d97a678a3231a3ea193b22b20c5
Baduit <simon.bertho@epitech.eu>
2019-10-02 19:16:55 +0200

    Operator function call with!!!

--- src/Type/Operators/Other/FunctionCall.hpp
+++ src/Type/Operators/Other/FunctionCall.hpp
@@ -7,6 +7,48 @@
 namespace Thuto
 {
 
+template <typename ...With>
+struct SfinaeHelperOperator_function_call_with
+{
+	template<typename T, typename _ = void>
+	struct FunctionCallWith: std::false_type {};
+
+	template<typename T>
+	struct FunctionCallWith
+	<
+		T,
+		std::conditional_t
+		<
+			false,
+			Details::SfinaeHelper
+			<
+				decltype(std::declval<T>()(std::declval<With...>()))
+			>,
+			void
+		>
+	> : public std::true_type {};
+
+};
+
+template <typename T, typename ...With>
+inline constexpr bool has_operator_function_call_with()
+{
+	using Type = SfinaeHelperOperator_function_call_with<With...>;
+	using FinalType = typename Type::template FunctionCallWith<T>;
+
+    if constexpr (std::is_base_of_v<std::true_type, FinalType>)
+        return true;
+    else
+        return false;
+}
+
+struct MyDummy {};
+
+void test_reve()
+{
+	has_operator_function_call_with<MyDummy, int, int>();
+}
+
 template<typename T, typename _ = void>
 struct SfinaeHasOperator_function_call : std::false_type {};
 
@@ -28,7 +70,7 @@ struct SfinaeHasOperator_function_call
 };
 
 
-template <typename T, typename With>
+template <typename T>
 inline constexpr bool has_operator_function_call()
 {
     if constexpr (std::is_base_of_v<std::true_type, SfinaeHasOperator_function_call<T>>)
@@ -36,7 +78,6 @@ inline constexpr bool has_operator_function_call()
     else
         return false;
 }
-
 
 
 }
--- test/Type/operator/other_main.cpp
+++ test/Type/operator/other_main.cpp
@@ -1,9 +1,24 @@
 #include <cassert>
+#include <string_view>
 #include <iostream>
 
 #include "HasOperator.hpp"
 
+struct Dummy
+{
+    auto operator()(std::string_view str) { return str.size(); }
+};
+
+void test_operator_function_call_with()
+{
+    assert((Thuto::has_operator_function_call_with<Dummy, int>() == false));
+    assert((Thuto::has_operator_function_call_with<Dummy, std::string_view>() == true));
+    assert((Thuto::has_operator_function_call_with<Dummy, std::string>() == true));
+    assert((Thuto::has_operator_function_call_with<Dummy, const char*>() == true));
+    assert((Thuto::has_operator_function_call_with<Dummy, Dummy>() == false));
+}
+
 int main()
 {
-    
+    test_operator_function_call_with();
 }
