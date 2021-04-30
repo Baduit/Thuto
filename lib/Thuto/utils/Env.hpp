c96f116a904e2f70f8b778fa7d7250eec4861441
Baduit <lena.bertho@protonmail.com>
2021-04-30 22:55:17 +0200

    Get env :D

--- lib/Thuto/utils/Env.hpp
+++ lib/Thuto/utils/Env.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
+#include <string>
+#include <optional>
 #include <string_view>
-#include <cstdlib>
 
 namespace thuto
 {
 
 // var_name must be null terminated
 // this is also the reason it doesn't use std::string_view
-std::string_view get_env(const char* var_name);
+std::optional<std::string_view> get_env(const char* var_name);
+
+std::optional<std::string_view> get_env(const std::string& var_name);
 
 } // namespace thuto
